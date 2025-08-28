f6a7289b8da534bba2a1d984fba010c4800fd46a
shanmugam200418 <shanmugamjayavelkmr@gmail.com>
2025-08-28 18:56:17 +0530

    Add files via upload

---
+++ pattern4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+ class pattern4{
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=row;col++){
+            System.out.print("*");
+            }
+            System.out.println();
+        }
+    for(int row=1;row<n;row++){
+        for(int col=1;col<=n-row;col++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+            
+    }
+ }
